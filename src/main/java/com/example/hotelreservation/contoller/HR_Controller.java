64d2e14c80a6c7670d6c0cce5aa717125acf32e2
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-07-23 15:15:39 +0400

    Controller

--- src/main/java/com/example/hotelreservation/contoller/HR_Controller.java
+++ src/main/java/com/example/hotelreservation/contoller/HR_Controller.java
@@ -1,2 +1,22 @@
-package com.example.hotelreservation.contoller;public class HR_Controller {
+package com.example.hotelreservation.contoller;
+
+import com.example.hotelreservation.dto.HR_Add_DTO;
+import com.example.hotelreservation.service.HR_Interface;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/api")
+public class HR_Controller {
+    private final HR_Interface hrInterface;
+
+    public HR_Controller(HR_Interface hrInterface) {
+        this.hrInterface = hrInterface;
+    }
+
+    @PostMapping(path = "/addHotel")
+    public String addHotel(@RequestBody HR_Add_DTO hrAddDto) {
+        hrInterface.addHotel(hrAddDto);
+        return "hotel added";
+    }
 }
