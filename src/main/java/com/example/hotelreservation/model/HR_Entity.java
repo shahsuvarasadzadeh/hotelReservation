8e3645ec074386cf5f06562bc87ef2ad04b9db7d
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-07-23 15:16:11 +0400

    Entity

---
+++ src/main/java/com/example/hotelreservation/model/HR_Entity.java
@@ -0,0 +1,25 @@
+package com.example.hotelreservation.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "Hotel_Reservation")
+@Data
+@AllArgsConstructor
+public class HR_Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private Long star;
+
+    public HR_Entity(String name,String location,Long star){
+        this.name=name;
+        this.location=location;
+        this.star=star;
+    }
+}
